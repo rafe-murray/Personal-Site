aa61b51d389b210e11085dff155c76682c5ede34
rafe <rafemurr@gmail.com>
2024-08-10 21:27:22 -0400

    Switch to using fontawesome icons

--- src/About.js
+++ src/About.js
@@ -4,7 +4,7 @@ import github from './assets/github.svg';
 import linkedin from './assets/linkedin.svg';
 import mail from './assets/mail-142.svg';
 import profilePicture from './assets/rafe-murray.png';
-// import './output.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Resume(){
     return (
@@ -14,8 +14,8 @@ function Resume(){
 
 function Link({title, href, icon}){
     return(
-        <a href={href} title={title} class="inline-block">
-            <img src={icon} class="text-lg" />
+        <a href={href} title={title} class="inline-block p-2">
+            <FontAwesomeIcon icon={icon} className='h-[3rem]'/>
         </a>
     );
 }
@@ -23,18 +23,18 @@ function Link({title, href, icon}){
 export default function About() {
     return (
         <div id="About">
-            <div class="max-w-screen-lg flex flex-row items-center mx-auto place-content-center">
+            <div class="max-w-screen-lg flex flex-row items-top mx-auto place-content-center">
                 <div class="w-2/3">
                     <h1 class="font-bold text-8xl p-4">Hi, I'm Rafe</h1>
                     <p class="p-4">I'm a computer engineering student looking to solve problems involving etcetera ... Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus blandit risus ut ex consequat, et aliquet arcu iaculis. Vestibulum eu eros et tellus porttitor ornare. Quisque dictum ex nibh. Suspendisse et elit et ante fringilla iaculis. Etiam posuere tortor risus, sit amet sollicitudin felis commodo vitae. Pellentesque vehicula ultrices dignissim. Nam rhoncus non lectus eget euismod. Maecenas in dapibus nibh, quis aliquet nisi. </p>
-                    <div class="p-4">
+                    <div class="p-4 items-center">
                         <Resume />
-                        <Link title='See my projects on Github' href='https://github.com/rafe-murray' icon ={github} />
-                        <Link title='View my profile on Linkedin' href='https://www.linkedin.com/in/rafe-murray' icon={linkedin} />
-                        <Link title='Email me' href='mailto:rafemurr@gmail.com' icon={mail} />
+                        <Link title='See my projects on Github' href='https://github.com/rafe-murray' icon ='fa-brands fa-github' />
+                        <Link title='View my profile on Linkedin' href='https://www.linkedin.com/in/rafe-murray' icon='fa-brands fa-linkedin' />
+                        <Link title='Email me' href='mailto:rafemurr@gmail.com' icon='fa-regular fa-envelope' />
                     </div>
                 </div>
-                <img src={profilePicture} class="w-1/3 h-128 p-4"/>
+                <img src={profilePicture} class="w-1/3 p-4 rounded-[2rem]"/>
             </div>
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ import './output.css'
 import App from './App';
 import About from './About'
 import reportWebVitals from './reportWebVitals';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+
+library.add( fab, faEnvelope);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
